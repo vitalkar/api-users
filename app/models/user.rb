24d9f62e3c3b0d4667c97c2f2a29c32c160ff7f1
vitalkar <vitalykar1@gmail.com>
2019-06-07 02:13:44 +0300

    added basic JWT setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    # has_secure_password
     validates :username, presence: true
     validates :password, presence: true, length: 6..20
     validates :email, presence: true, format: /@/
